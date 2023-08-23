c6577d46cae65bd342909394743d7d30fe967abd
Eduardo Bartichoto <eduardobarti@gmail.com>
2023-08-23 01:09:57 -0300

    continuando estilização container, fonts e  input, e criação js

---
+++ assets/js/cria-tasks.js
@@ -0,0 +1,11 @@
+/* window.onload = function() {
+    document.getElementById('input-disabled').disabled = true;
+    document.getElementById('input-readonly').readOnly = true;
+}
+
+  <input type="text" id="input-disabled">
+  <input type="text" id="input-readonly">
+
+*/
+
+// Opção para ativar e desativar edição no input da task item.
