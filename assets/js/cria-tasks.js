8224034ccec9e63dc2441bcdf1a5e667ff6bff4a
Eduardo Bartichoto <eduardobarti@gmail.com>
2023-08-25 00:55:17 -0300

    iniciando implementação da criação dos itens da lista com JS

--- assets/js/cria-tasks.js
+++ assets/js/cria-tasks.js
@@ -9,3 +9,48 @@
 */
 
 // Opção para ativar e desativar edição no input da task item.
+var inputTask = document.querySelector("#input__box");
+var buttonInputTask = document.querySelector(".input__button");
+
+function criaTask(task) {
+  // cria elementos HTML
+  var itemList = document.createElement('div');
+  var inputItem = document.createElement('input');
+  var buttonEdit = document.createElement("button");
+  var buttonSave = document.createElement("button");
+  var buttonDel = document.createElement("button");
+
+  //busca pai dos elementos
+  var listOfItems = document.querySelector(".list__items");
+
+  //adiciona classe aos elementos
+  itemList.classList.add("list__item");
+  inputItem.classList.add("item__input");
+  buttonEdit.classList.add("item__button-edit");
+  buttonSave.classList.add("item__button-save");
+  buttonDel.classList.add("item__button-delete");
+
+  //Insere textContent dos botões criados.
+  buttonEdit.textContent = "edit";
+  buttonSave.textContent = "save";
+  buttonDel.textContent = "delete";
+
+  listOfItems.appendChild(itemList);
+  itemList.appendChild(inputItem);
+  itemList.appendChild(buttonEdit);
+  itemList.appendChild(buttonSave);
+  itemList.appendChild(buttonDel);
+
+  inputItem.value = task;
+}
+
+function ocultaEmptyList () {
+  var emptyList = document.querySelector(".list__empty");
+  emptyList.style.display = "none";
+}
+
+buttonInputTask.addEventListener("click", function(){
+   var task = inputTask.value;
+   criaTask(task);
+   ocultaEmptyList();
+})
