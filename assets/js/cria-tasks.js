2dd6d883c86937d6148af79c336dae012fbca8bb
Eduardo Bartichoto <eduardobarti@gmail.com>
2023-08-26 00:40:24 -0300

    implementação da funcionalidade de edição da task criada

--- assets/js/cria-tasks.js
+++ assets/js/cria-tasks.js
@@ -1,14 +1,3 @@
-/* window.onload = function() {
-    document.getElementById('input-disabled').disabled = true;
-    document.getElementById('input-readonly').readOnly = true;
-}
-
-  <input type="text" id="input-disabled">
-  <input type="text" id="input-readonly">
-
-*/
-
-// Opção para ativar e desativar edição no input da task item.
 var inputTask = document.querySelector("#input__box");
 var buttonInputTask = document.querySelector(".input__button");
 
@@ -19,6 +8,9 @@ function criaTask(task) {
   var buttonEdit = document.createElement("button");
   var buttonSave = document.createElement("button");
   var buttonDel = document.createElement("button");
+
+  //desabilita input do item.
+  inputItem.disabled = true;
 
   //busca pai dos elementos
   var listOfItems = document.querySelector(".list__items");
@@ -42,6 +34,33 @@ function criaTask(task) {
   itemList.appendChild(buttonDel);
 
   inputItem.value = task;
+
+  editaTask();
+}
+
+function editaTask() {
+  var buttonsEdit = document.querySelectorAll(".item__button-edit");
+  var buttonsSave = document.querySelectorAll(".item__button-save");
+  var inputsItem = document.querySelectorAll(".item__input");
+
+  for (let i = 0; i < buttonsEdit.length; i++) {
+    const edit = buttonsEdit[i];
+    const save = buttonsSave[i];
+    const input = inputsItem[i];
+
+    edit.addEventListener("click", function () {
+      input.disabled = false;
+      input.focus();
+      edit.style.display = "none";
+      save.style.display = "inline-block";
+    })
+
+    save.addEventListener("click", function (){
+      input.disabled = true;
+      edit.style.display = "inline-block";
+      save.style.display = "none";
+    })
+  }
 }
 
 function ocultaEmptyList () {
@@ -50,7 +69,9 @@ function ocultaEmptyList () {
 }
 
 buttonInputTask.addEventListener("click", function(){
-   var task = inputTask.value;
-   criaTask(task);
-   ocultaEmptyList();
+  var task = inputTask.value;
+  criaTask(task);
+  inputTask.value = "";
+  inputTask.focus();  
+  ocultaEmptyList();
 })
